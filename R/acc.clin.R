4f191d107dfb490ac2ffd614828c6be94d445120
Giorgio Montesi <Giorgio@MacBook-Pro-di-Giorgio-2.local>
2024-12-10 15:02:36 -0300

    Added source info to internal datasets

--- R/acc.clin.R
+++ R/acc.clin.R
@@ -4,4 +4,5 @@
 #' Dataset containing clinical information of ACC samples. Samples are on rows and features are on columns.
 #'
 #' @format 92 rows and 9 columns
+#' @source https://gdac.broadinstitute.org/runs/stddata__2016_01_28/data/ACC/20160128/
 "acc.clin"
--- R/acc.count.R
+++ R/acc.count.R
@@ -3,4 +3,5 @@
 #' @description
 #' Count table of ACC data. Samples are on rows and features are on columns.
 #' @format 79 rows and 20502 columns.
+#' @source https://gdac.broadinstitute.org/runs/stddata__2016_01_28/data/ACC/20160128/
 "acc.count"
